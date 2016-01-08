76541967acbfd0392f5bc92cd008be57c032899c
ybrs <aybars.badur@gmail.com>
2016-01-08 17:39:44 +0200

    moves

--- lemmingz/GameScene.swift
+++ lemmingz/GameScene.swift
@@ -11,65 +11,168 @@ import SpriteKit
 enum CollisionTypes: UInt32 {
     case Player = 1
     case Wall = 2
-    case Star = 4
-    case Vortex = 8
-    case Finish = 16
+    case Enter = 4
+    case Exit = 8
 }
 
-class GameScene: SKScene {
+class Hero: SKSpriteNode {
+    var _running = false
+    var _direction = CGRectEdge.MaxXEdge
+    
+    var running : Bool {
+        get {
+            return self._running
+        }
+        set (newValue) {
+            if self._running == newValue {
+                return
+            }
+            
+            self._running = newValue
+            
+            updateActions()
+        }
+    }
+    
+    var direction: CGRectEdge {
+        get {
+            return _direction
+        }
+        set (newDirection) {
+            if self._direction == newDirection {
+                return
+            }
+            
+            self._direction = newDirection
+            updateActions()
+        }
+    }
+    
+    func updateActions() {
+        if !self._running {
+            self.removeActionForKey("running")
+            self.removeActionForKey("run")
+        } else if self.parent != nil {
+            let heroAtlas = SKTextureAtlas(named: "sprites.atlas")
+
+            if self.direction == .MaxXEdge {
+                let hero_run_anim = SKAction.animateWithTextures([
+                    heroAtlas.textureNamed("sprite_01"),
+                    heroAtlas.textureNamed("sprite_02"),
+                    heroAtlas.textureNamed("sprite_03")
+                    ], timePerFrame: 0.06)
+
+                let run = SKAction.repeatActionForever(hero_run_anim)
+                self.runAction(run, withKey: "running")
+            } else if self.direction == .MinXEdge {
+                let hero_run_anim = SKAction.animateWithTextures([
+                    heroAtlas.textureNamed("sprite_17"),
+                    heroAtlas.textureNamed("sprite_18"),
+                    heroAtlas.textureNamed("sprite_19")
+                    ], timePerFrame: 0.06)
+
+                let run = SKAction.repeatActionForever(hero_run_anim)
+                self.runAction(run, withKey: "running")
+            }
+            
+            
+            let delta = self.direction == CGRectEdge.MinXEdge ? -50 : 50;
+            let move = SKAction.moveBy(CGVector(dx: delta, dy: 0), duration: 1)
+            let moveForever = SKAction.repeatActionForever(move)
+            self.runAction(moveForever, withKey: "move")
+        }
+    }
+   
+}
+
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
     var enterNode: SKSpriteNode!
     
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
 
-        self.physicsWorld.gravity = CGVectorMake(0.0, -0.2)
-
-        
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!"
-        myLabel.fontSize = 45
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
+        self.physicsWorld.gravity = CGVectorMake(0.0, -2.0)
+        self.physicsWorld.contactDelegate = self
         
         view.autoresizingMask = [.FlexibleHeight, .FlexibleWidth]
 
-        self.addChild(myLabel)
         loadLevel()
         
         addSprites()
         
     }
     
+    func didEndContact(contact: SKPhysicsContact) {
+//        NSLog("%@", contact.bodyA)
+//        NSLog("%@", contact.bodyB)
+//        NSLog("%@", NSStringFromCGPoint(contact.contactPoint))
+//        NSLog("%@", NSStringFromCGVector(contact.contactNormal))
+//        NSLog("%f", contact.collisionImpulse)
+        
+        let hero:SKNode = contact.bodyB.node!
+        if hero.actionForKey("run") == nil {
+            hero.removeActionForKey("run")
+            
+            hero.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
+        }
+    }
+    
+    func didBeginContact(contact: SKPhysicsContact) {
+//        NSLog("%@", contact.bodyA)
+        NSLog("%@", contact.bodyB)
+//        NSLog("%@", NSStringFromCGPoint(contact.contactPoint))
+        NSLog("begin contact %@", NSStringFromCGVector(contact.contactNormal))
+//        NSLog("%f", contact.collisionImpulse)
+        
+        let node:SKNode = contact.bodyB.node!
+        if let hero = node as? Hero {
+            if hero.name == "hero" {
+                if contact.contactNormal.dy == 1 {
+                    NSLog("same direction no change")
+                    hero.running = true
+                } else if contact.contactNormal.dx == -1  {
+                    // Hit by right
+                    NSLog("change direction right")
+                    hero.direction = CGRectEdge.MinXEdge
+                    
+                } else if contact.contactNormal.dx == 1 {
+                    NSLog("change direction left")
+                    
+                    hero.direction = CGRectEdge.MaxXEdge
+                }
+            }
+        }
+        
+    }
+    
     func addSprites(){
         let heroAtlas = SKTextureAtlas(named: "sprites.atlas")
-        let hero = SKSpriteNode(texture: heroAtlas.textureNamed("sprite_01"))
+        let hero = Hero(texture: heroAtlas.textureNamed("sprite_01"))
+        hero.direction = CGRectEdge.MaxXEdge
+        hero.name = "hero"
         hero.position = self.enterNode.position
         hero.size = CGSize(width: 32, height: 32)
-        hero.physicsBody = SKPhysicsBody(rectangleOfSize: hero.size)
+        hero.physicsBody = SKPhysicsBody(rectangleOfSize: CGSize(width: 10.0, height: 32.0))
+        
         hero.physicsBody!.categoryBitMask = CollisionTypes.Player.rawValue
+        hero.physicsBody!.contactTestBitMask = CollisionTypes.Wall.rawValue
+//        hero.physicsBody!.collisionBitMask = CollisionTypes.Wall.rawValue
+        
+        
         hero.physicsBody!.dynamic = true
         hero.physicsBody!.allowsRotation = false
-        hero.physicsBody!.restitution = 1
+        hero.physicsBody!.restitution = 0.0
         hero.physicsBody!.linearDamping = 0
         hero.physicsBody!.friction = 0
         hero.zPosition = 1.0
 
-        hero.physicsBody!.velocity = CGVectorMake(100, 0)
-        hero.physicsBody!.applyImpulse(CGVectorMake(0.1, 0.1))
+//        hero.physicsBody!.velocity = CGVectorMake(100, 0)
+//        hero.physicsBody!.applyImpulse(CGVectorMake(0.1, 0.1))
 
-        
-        let hero_run_anim = SKAction.animateWithTextures([
-            heroAtlas.textureNamed("sprite_01"),
-            heroAtlas.textureNamed("sprite_02"),
-            heroAtlas.textureNamed("sprite_03")
-            ], timePerFrame: 0.06)
-        
-        let run = SKAction.repeatActionForever(hero_run_anim)
-        
         
         
         addChild(hero)
-        hero.runAction(run, withKey: "running")
 
 //        let moveRight = SKAction.moveByX(10, y: 0, duration: 20)
 //        let moveForever = SKAction.repeatActionForever(moveRight)
@@ -113,7 +216,7 @@ class GameScene: SKScene {
                             node.size = CGSize(width: blockSize, height: blockSize)
                             
                             node.physicsBody = SKPhysicsBody(rectangleOfSize: node.size)
-                            node.physicsBody!.categoryBitMask = CollisionTypes.Wall.rawValue
+                            node.physicsBody!.categoryBitMask = CollisionTypes.Exit.rawValue
                             node.physicsBody!.dynamic = false
                             addChild(node)
                         } else if letter == "E"  {
@@ -124,7 +227,7 @@ class GameScene: SKScene {
                             self.enterNode.size = CGSize(width: blockSize, height: blockSize)
                             
                             self.enterNode.physicsBody = SKPhysicsBody(rectangleOfSize: self.enterNode.size)
-                            self.enterNode.physicsBody!.categoryBitMask = CollisionTypes.Wall.rawValue
+                            self.enterNode.physicsBody!.categoryBitMask = CollisionTypes.Enter.rawValue
                             self.enterNode.physicsBody!.dynamic = false
                             addChild(self.enterNode)
 
